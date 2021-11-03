f80411ce5a3696f4e92a6d5a5f5dcbd1d3567815
virustotalop <virustotalop@gmail.com>
2021-11-03 04:22:25 -0700

    Try to clarify debouncer javadoc

--- src/main/java/com/gmail/virustotalop/equipment/Debouncer.java
+++ src/main/java/com/gmail/virustotalop/equipment/Debouncer.java
@@ -3,13 +3,13 @@ package com.gmail.virustotalop.equipment;
 public interface Debouncer<K> {
 
     /**
-     * @param key K to check if the key will debounced
+     * @param key K to check if the key would debounce
      * @return if the key was debounced
      */
     boolean wouldDebounce(K key);
 
     /**
-     * @param key K to save the key for debouncing
+     * @param key K to save for debouncing
      * @return if the key was debounced
      */
     boolean debounce(K key);
