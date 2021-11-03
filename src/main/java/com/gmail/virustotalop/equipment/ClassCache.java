d051e38819a1c8b1a72e538a3f25a80c51b7a79a
virustotalop <virustotalop@gmail.com>
2021-11-03 04:26:46 -0700

    Add optional class loader for looking up classes

--- src/main/java/com/gmail/virustotalop/equipment/ClassCache.java
+++ src/main/java/com/gmail/virustotalop/equipment/ClassCache.java
@@ -8,13 +8,22 @@ public interface ClassCache {
     Map<String, Optional<Class<?>>> cache();
 
     default Class<?> lookup(String... classes) {
+        return this.lookup(null, classes);
+    }
+
+    default Class<?> lookup(ClassLoader classLoader, String... classes) {
         for(String clazzName : classes) {
             Optional<Class<?>> clazzOpt = this.cache().get(clazzName);
             if(clazzOpt != null && clazzOpt.isPresent()) {
                 return clazzOpt.get();
             }
             try {
-                Class<?> clazz = Class.forName(clazzName);
+                Class<?> clazz;
+                if(classLoader == null) {
+                    clazz = Class.forName(clazzName);
+                } else {
+                    clazz = Class.forName(clazzName, true, classLoader);
+                }
                 clazzOpt = Optional.of(clazz);
                 this.cache().put(clazzName, clazzOpt);
                 return clazz;
