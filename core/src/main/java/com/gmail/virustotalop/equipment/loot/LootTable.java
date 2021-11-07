8a1e86cc851fe7a6b7c330da42bef912fb9026d3
virustotalop <virustotalop@gmail.com>
2021-11-06 22:37:04 -0700

    Start implementing the loot table

--- core/src/main/java/com/gmail/virustotalop/equipment/loot/LootTable.java
+++ core/src/main/java/com/gmail/virustotalop/equipment/loot/LootTable.java
@@ -2,33 +2,65 @@ package com.gmail.virustotalop.equipment.loot;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
-public class LootTable {
+public class LootTable<T extends Loot> {
 
-    private final Map<Loot, Integer> table;
+    private final Map<Integer, Map.Entry<T, Integer>> table;
     private final int scale;
 
-    public LootTable(@NotNull Map<Loot, Integer> table) {
+    public LootTable(@NotNull Map<T, Integer> table) {
         this(table, 100);
     }
 
-    public LootTable(@NotNull Map<Loot, Integer> table, int scale) {
-        this.table = table;
+    public LootTable(@NotNull Map<T, Integer> table, int scale) {
+        this.table = this.loadLoot(table);
         this.scale = scale;
     }
 
-    public boolean roll() {
+    private Map<Integer, Map.Entry<T, Integer>> loadLoot(Map<T, Integer> table) {
+        Map<Integer, Map.Entry<T, Integer>> indexedMap = new HashMap<>();
+        int index = 0;
+        for(Map.Entry<T, Integer> next : table.entrySet()) {
+            indexedMap.put(index, next);
+            index += 1;
+        }
+        return indexedMap;
+    }
+
+    public Collection<T> roll() {
         return this.roll(0);
     }
 
-    public boolean roll(int guaranteed) {
+    public Collection<T> roll(int guaranteed) {
         return this.roll(guaranteed, 0);
     }
 
-    private boolean roll(int guaranteed, int currentRolls) {
-        //TODO - implement
-        return false;
+    private Collection<T> roll(int guaranteed, int generatedRolls) {
+        Collection<T> loot = new ArrayList<>();
+        while(guaranteed < generatedRolls) {
+            Random rand = new Random();
+            T rolled = this.rollIndex(rand.nextInt(this.table.size()));
+            if(rolled != null) {
+                loot.add(rolled);
+                generatedRolls += 1;
+            }
+        }
+        return loot;
+    }
+
+    private T rollIndex(int index) {
+        Map.Entry<T, Integer> entry = this.table.get(index);
+        Random rand = new Random();
+        int next = rand.nextInt(this.getScale()) + 1;
+        if(next <= entry.getValue()) {
+            return entry.getKey();
+        }
+        return null;
     }
 
     public int getScale() {
